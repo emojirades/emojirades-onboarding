16b925a9c97cf29579f8c23406ec20428ddbcc98
Michael Robbins <michael@dalmura.com.au>
2020-08-14 22:17:57 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # emojirades-auth
 OAuth Server for the Emojirades slackbot
+
+## Usage
+Works over CLI or through AWS Lambda
+
+
+```bash
+# Running over CLI
+
+```
+
+
+
+## How it works
+Short explaination about slack OAuth process
+
+How we use DynamoDB for the state persistence
+How we use SNS for the game bot reconfiguration
---
+++ src/handler.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import boto3
+import uuid
+import os
+
+
+base_url = os.environ.get("BASE_URL", "https://slack.com/oauth/authorize")
+client_id = os.environ["CLIENT_ID"]
+scope = os.environ["SCOPE"]
+
+
+dynamo = boto3.client("dynamodb")
+sqs = boto3.client("sqs")
+
+
+# Generic
+def setup():
+    state = uuid.uuid4()
+    # Persist state to DynamoDB with 10 min TTL
+
+    return {
+        "action": "redirect",
+        "url": f"{base_url}?client_id={client_id}&scope={scope}&state={state}",
+    }
+
+
+def onboard(code, state):
+    # Get state from DynamoDB
+    # Return failure if it doesn't exist
+
+    # Use code to retrieve auth creds
+    # Return failure if it's incorrect
+
+    # Save auth.json in S3 if valid creds
+    # Submit SQS event for game bots to reconfigure
+    pass
+
+
+
+# Lambda Hander
+def handler(event, context):
+    pass
+
+
+
+# CLI Handler
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
