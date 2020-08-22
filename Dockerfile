2f8c15cea4aa29151559cc6aa07e8aee06dd54b0
Michael Robbins <michael.robbins@servian.com>
2020-08-22 15:44:43 +1000

    Fleshed out the second half of the onboarding flow

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,15 @@ FROM python:3.8
 
 RUN apt-get update && apt-get install zip
 
-# Copy in code
-WORKDIR /src
-COPY requirements.txt .
-COPY src code/
-
 # Build dependencies into zip
-#WORKDIR /src/package
-#RUN pip3 install --upgrade --no-cache-dir -r ../requirements.txt --target .
-#RUN zip --quiet -r9 ../function.zip .
+WORKDIR /src/package
+COPY requirements.txt .
+RUN pip3 install --upgrade --no-cache-dir -r requirements.txt --target . && rm requirements.txt
+RUN zip --quiet -r9 ../function.zip .
 
 # Build source into zip
 WORKDIR /src/code
+COPY src code/
 RUN zip --quiet -g -r ../function.zip *
 
 WORKDIR /src
--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import requests
 import datetime
 import boto3
 import json
@@ -11,6 +12,7 @@ import os
 # Boto3 clients
 dynamo = boto3.client("dynamodb")
 sqs = boto3.client("sqs")
+s3 = boto3.client("s3")
 secrets = boto3.client("secretsmanager")
 
 
@@ -18,10 +20,14 @@ secrets = boto3.client("secretsmanager")
 environment = os.environ["ENVIRONMENT"]
 secret_arn = os.environ["SECRET_ARN"]
 state_table = os.environ["STATE_TABLE"]
+auth_bucket = os.environ["AUTH_BUCKET"]
+queue_url = os.environ["QUEUE_URL"]
 
 # Optional env vars
-base_url = os.environ.get("BASE_URL", "https://slack.com/oauth/authorize")
+base_auth_url = os.environ.get("BASE_AUTH_URL", "https://slack.com/oauth/authorize")
+base_access_url = os.environ.get("BASE_ACCESS_URL", "https://slack.com/api/oauth.access")
 state_ttl_delta = int(os.environ.get("STATE_TTL_DELTA", "300"))
+auth_bucket_key = os.environ.get("AUTH_BUCKET_KEY", "teams/{team_id}/auth.json")
 
 # Get secret config
 secret = json.loads(secrets.get_secret_value(SecretId=secret_arn)["SecretString"])
@@ -31,9 +37,48 @@ client_secret = secret["CLIENT_SECRET"]
 scope = secret["SCOPE"]
 
 
+def build_response(status_code=200, headers=None, body=None):
+    response = {
+        "isBase64Encoded": False,
+        "statusCode": status_code,
+    }
+
+    if headers is not None:
+        response["headers"] = headers
+
+    if body is not None:
+        response["body"] = body
+
+    return response
+
+
+def build_redirect_response(url):
+    headers = {
+        "Location": url,
+        "Referrer-Policy": "no-referrer",
+    }
+
+    return build_response(
+        status_code=302,
+        headers=headers,
+    )
+
+
+def build_message_response(message, **kwargs):
+    if kwargs.get("headers"):
+        kwargs["headers"]["Content-Type"] = "application/json"
+    else:
+        kwargs["headers"] = {"Content-Type": "application/json"}
+
+    return build_response(
+        body=json.dumps({"message": message}),
+        **kwargs,
+    )
+
+
 def initiate(epoch_seconds):
     state_key = str(uuid.uuid4())
-    state_ttl = epoch_seconds + state_ttl_delta
+    state_ttl = str(epoch_seconds + state_ttl_delta)
 
     response = dynamo.put_item(
         TableName=state_table,
@@ -46,31 +91,78 @@ def initiate(epoch_seconds):
         ReturnItemCollectionMetrics="NONE",
     )
 
-    return {
-        "isBase64Encoded": False,
-        "statusCode": 302,
-        "headers": {
-            "Location": f"{base_url}?client_id={client_id}&scope={scope}&state={state_key}",
-            "Referrer-Policy": "no-referrer",
-        }
-    }
+    return build_redirect_response(f"{base_auth_url}?client_id={client_id}&scope={scope}&state={state_key}")
 
+def onboard(code, state_key):
+    # Verify that the state_key is still within the DynamoDB table
+    response = dynamo.get_item(
+        TableName=state_table,
+        Key={
+            "StateKey": {"S": state_key},
+        },
+        ReturnConsumedCapacity="NONE",
+    )
 
-def onboard(code, state):
-    # Get state from DynamoDB
-    # Return failure if it doesn't exist
+    if "Item" not in response:
+        return build_message_response("Onboarding flow has timed out, please authenticate again", status_code=400)
 
     # Use code to retrieve auth creds
-    # Return failure if it's incorrect
+    response = requests.post(
+        base_access_url,
+        data={
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "code": code,
+        }
+    )
 
-    # Save auth.json in S3 if valid creds
-    # Submit SQS event for game bots to reconfigure
+    if response.status_code != response.requests.codes.ok:
+        return build_message_response("Provided Slack credentials are invalid")
 
-    return {
-        "isBase64Encoded": False,
-        "statusCode": 200,
-        "body": json.dumps({"message": "Nothing has been done"}),
+    output = response.json()
+
+    if not output or not output.get("ok"):
+        return build_message_response("Slack response wasn't valid")
+
+    if "bot" not in output:
+        return build_message_response("Slack response missing the bot scope")
+
+    # Persist auth tokens to S3
+    team_name = output["team_name"]
+
+    auth_document = {
+        "team_id": output["team_id"],
+        "access_token": output["access_token"],
+        "bot_user_id": output["bot"]["bot_user_id"],
+        "bot_access_token": output["bot"]["bot_access_token"],
     }
+
+    s3.put_object(
+        Body=json.dumps(auth_document),
+        Bucket=auth_bucket,
+        ContentType="application/json",
+        Key=auth_bucket_key.format(team_id=team_id),
+    )
+
+    # Submit SQS event for game bot(s) to reconfigure
+    sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps({"team_id": team_id}),
+    )
+
+    # Remove our state_key to stop any replays
+    response = dynamo.delete_item(
+        TableName=state_table,
+        Key={
+            "StateKey": {"S": state_key},
+        },
+        ReturnValues="NONE",
+        ReturnConsumedCapacity="NONE",
+        ReturnItemCollectionMetrics="NONE",
+    )
+
+    # Let the user know they're good to go
+    return build_message_response(f"Successfully onboarded {team_name} to Emojirades!")
 
 
 def lambda_handler(event, context):
@@ -84,31 +176,17 @@ def lambda_handler(event, context):
         parameters = event.get("queryStringParameters")
 
         if not parameters:
-            return {
-                "isBase64Encoded": False,
-                "statusCode": 200,
-                "body": json.dumps({"message": "Missing code or state"}),
-            }
+            return build_message_response("Missing code or state parameters")
 
         code = parameters.get("code")
         state = parameters.get("state")
 
         if code is None or state is None:
-            return {
-                "isBase64Encoded": False,
-                "statusCode": 200,
-                "body": json.dumps({"message": "Missing code or state"}),
-            }
+            return build_message_response("Missing code or state parameters")
 
-        return onboard(None, None)
+        return onboard(code, state)
     else:
-        return {
-            "isBase64Encoded": False,
-            "statusCode": 302,
-            "headers": {
-                "Location": "https://emojirades.io",
-            }
-        }
+        return build_redirect_response("https://emojirades.io")
 
 
 def cli_handler():
